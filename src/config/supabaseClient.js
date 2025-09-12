8e057beb680863c5d82a88f9d924d89971055272
Ridwan Yusoff <ridwan@diveanalytics.com>
2025-09-13 03:49:52 +0800

    Update Supabase env vars and client

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://jhbknobdqwcfdxvfnxgb.supabase.co'
+// const supabaseUrl = 'https://jhbknobdqwcfdxvfnxgb.supabase.co'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_PROJECT_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey, {db: { schema: 'manganui' }})
